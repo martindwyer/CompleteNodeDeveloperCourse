75f7513dbc478b5efb52ae5487955362d56843de
Martin Dwyer <martin.dwyer@outlook.com>
2021-11-08 11:51:11 -0600

    App Updates

---
+++ task-manager/src/config/dev.js
@@ -0,0 +1,10 @@
+// dev.js - don't commit
+
+// Production db string "mongodb+srv://dbUser:j9IWgxVnLnqD9hnx@cluster0-2jhty.mongodb.net/test?retryWrites=true&w=majority"
+module.exports = {
+  port: 3000,
+  jwtSecretKey: 'completenodedevelopercourse',
+  sendGridAPI:
+    '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',
+  mongoURL: 'mongodb://127.0.0.1:27017/task-manager-api'
+}
---
+++ weather-app/config/dev.js
@@ -0,0 +1,6 @@
+// dev.js - don't commit
+
+// Production db string "mongodb+srv://dbUser:j9IWgxVnLnqD9hnx@cluster0-2jhty.mongodb.net/test?retryWrites=true&w=majority"
+module.exports = {
+  weatherStackAPI: 'aafec964702a0d8e44d77a02850927fc'
+}
